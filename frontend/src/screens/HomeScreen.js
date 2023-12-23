d6e25a730bd6bc6a476bade8dca50f00e3814e1e
samuel-eric <samueltan086@gmail.com>
2023-12-23 19:54:27 +0700

    initialize redux and make api slice

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,29 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../components/Product.js'
+import { useGetProductsQuery } from '../slices/productApiSlice.js'
 
 const HomeScreen = () => {
-	const [products, setProducts] = React.useState([])
-
-	React.useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products')
-			setProducts(data)
-		}
-
-		fetchProducts()
-	}, [])
+	const { data: products, isLoading, error } = useGetProductsQuery()
 
 	return (
 		<>
-			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{isLoading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<div>{error?.data?.message || error.error}</div>
+			) : (
+				<>
+					<h1>Latest Products</h1>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</>
+			)}
 		</>
 	)
 }
