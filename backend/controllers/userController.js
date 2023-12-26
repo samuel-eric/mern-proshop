955fcc245f7bfa0a26e9c627519e6c3b0af08464
samuel-eric <samueltan086@gmail.com>
2023-12-26 10:07:28 +0700

    tambah register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
-import jwt from 'jsonwebtoken'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -11,18 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
 	const user = await User.findOne({ email })
 
 	if (user && (await user.matchPassword(password))) {
-		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-			expiresIn: '30d',
-		})
-
-		// Set JWT as HTTP-Only cookie
-		res.cookie('jwt', token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV !== 'development',
-			sameSite: 'strict',
-			maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-		})
-
+		generateToken(res, user._id)
 		res.json({
 			_id: user._id,
 			name: user.name,
@@ -39,7 +28,29 @@ const authUser = asyncHandler(async (req, res) => {
 // @route POST /api/users
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
-	res.send('register user')
+	const { name, email, password } = req.body
+
+	const userExist = await User.findOne({ email })
+
+	if (userExist) {
+		res.status(400)
+		throw new Error('User already exist')
+	}
+
+	const user = await User.create({ name, email, password })
+
+	if (user) {
+		generateToken(res, user._id)
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
 })
 
 // @desc Logout user / clear cookie
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,10 +13,7 @@ import {
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router
-	.route('/')
-	.post(protect, admin, registerUser)
-	.get(protect, admin, getUsers)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/logout', logoutUser)
 router.post('/auth', authUser)
 router
