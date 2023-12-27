27cf7cee267e4f7c05219eeff4e0f04f18b4a4b3
samuel-eric <samueltan086@gmail.com>
2023-12-27 14:51:36 +0700

    tambah checkout steps

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
-import { UseSelector, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const PrivateRoute = () => {
 	const { userInfo } = useSelector((state) => state.auth)
